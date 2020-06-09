b29c34e9f554acb907a12fce0396368d4c2f6a29
Lakindu <lakinduakash@gmail.com>
2020-06-09 22:11:21 +0530

    Remove unnecessary checks

--- api/utilities/dbConnection.ts
+++ api/utilities/dbConnection.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 export async function dbConnect() {
     let uri = "mongodb://localhost:27017/test";
-    if (uri == undefined) {
-        throw new Error('environment variable undefined : mongodb uri');
+    if (!uri) {
+        throw new Error('Mongodb uri not defined');
     }
     try {
         await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
