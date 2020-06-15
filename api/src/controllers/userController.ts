ecd38e0f26f67a2224b8a007a9c9326a8a5c7c2c
Lakindu <lakinduakash@gmail.com>
2020-06-15 09:53:05 +0530

    Add user to db

---
+++ api/src/controllers/userController.ts
@@ -0,0 +1,36 @@
+import User, {IUser} from "../models/userModel"
+import {MethodResponse, ResponseStatusCode} from "../types/commonTypes";
+import {v4 as uuid} from 'uuid';
+import {GeneralUserRegistrationParams} from "../types/authApiTypes";
+
+export class UserController {
+
+    public static async addUser(userInfo: GeneralUserRegistrationParams): Promise<MethodResponse> {
+        try {
+            let isDuplicate = await User.exists({user: userInfo.user});
+            if (isDuplicate) {
+                return new MethodResponse(ResponseStatusCode.BadRequest, 'The user already exists');
+            }
+            const _uuid: string = uuid();
+            const newUser: IUser = new User({
+                user: userInfo.user,
+                identity: userInfo.identity,
+                uuid: _uuid
+            });
+            await newUser.save();
+            return new MethodResponse(ResponseStatusCode.Okay, 'okay')
+            // try {
+            //     let jwtToken = await createJWT({
+            //         _id : newUser._id,
+            //         uuid: newUser.uuid
+            //     });
+            //     return new MethodResponse(ResponseStatusCode.Okay, 'okay', jwtToken);
+            // } catch (why) {
+            //     return new MethodResponse(ResponseStatusCode.InternalError, why)
+            // }
+        } catch (why) {
+            return new MethodResponse(ResponseStatusCode.InternalError, why)
+        }
+    }
+}
+
