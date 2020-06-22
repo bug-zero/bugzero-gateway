272a298d3312fcb92545907a6f76899a85503bc4
Lakindu <lakinduakash@gmail.com>
2020-06-22 11:34:02 +0530

    Add route for list users

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -1,6 +1,22 @@
+import {MethodResponse} from '../types/commonTypes';
+import {UserController} from '../controllers/userController';
 import {Router} from 'express';
 
 export const userRouter: Router = Router();
+
+// userRouter.post("/api/adduser", async (request, response) => {
+//     let user: string = String(request.body.user);
+//     let identity: string = String(request.body.identity);
+//     let addUserResponse: MethodResponse = await UserController.
+// })
+
+userRouter.get("/api/userlist", async (_request, response) => {
+    let controllerResponse: MethodResponse = await UserController.getAllUsers();
+    response.status(controllerResponse.status).send({
+        responseMessage: controllerResponse.responseMessage,
+        payload: controllerResponse.payload
+    });
+});
 
 
 //add the api/deleteuser route
