cffd2b4bd1c127c2a55242ded878d02aeb494f67
Lakindu Akash <lakinduakash@gmail.com>
2020-08-16 05:07:27 +0530

    util to get CA

--- api/src/routes/utilRoutes.ts
+++ api/src/routes/utilRoutes.ts
@@ -25,3 +25,63 @@ utilRouter.get("/status", async (_request, response) => {
         handleAndLogErrors(e, response)
     }
 })
+
+utilRouter.get("/getca", async (_request, response) => {
+
+    try {
+        let res = await VpnUtilOperations.getCACert()
+        if (res.code == null || res.code == 0) {
+            response.status(ResponseStatusCode.Okay).send({
+                responseMessage: 'success',
+                payload: res.stdout
+            });
+        } else {
+            response.status(ResponseStatusCode.InternalError).send({
+                responseMessage: 'fail',
+                payload: res.stderr
+            });
+        }
+    } catch (e) {
+        handleAndLogErrors(e, response)
+    }
+})
+
+utilRouter.get("/getcert", async (_request, response) => {
+
+    try {
+        let res = await VpnUtilOperations.getCert()
+        if (res.code == null || res.code == 0) {
+            response.status(ResponseStatusCode.Okay).send({
+                responseMessage: 'success',
+                payload: res.stdout
+            });
+        } else {
+            response.status(ResponseStatusCode.InternalError).send({
+                responseMessage: 'fail',
+                payload: res.stderr
+            });
+        }
+    } catch (e) {
+        handleAndLogErrors(e, response)
+    }
+})
+
+utilRouter.get("/getubuntuclientconfig", async (_request, response) => {
+
+    try {
+        let res = await VpnUtilOperations.getUbuntuClientConfig()
+        if (res.code == null || res.code == 0) {
+            response.status(ResponseStatusCode.Okay).send({
+                responseMessage: 'success',
+                payload: res.stdout
+            });
+        } else {
+            response.status(ResponseStatusCode.InternalError).send({
+                responseMessage: 'fail',
+                payload: res.stderr
+            });
+        }
+    } catch (e) {
+        handleAndLogErrors(e, response)
+    }
+})
