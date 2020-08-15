6a80b167b6a512af601d060bdbd1b6738d15bfa1
Lakindu Akash <lakinduakash@gmail.com>
2020-08-16 02:30:09 +0530

    Remove mongo connect

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,14 +8,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {userRouter} from "./routes/userRoutes";
 import {checkToken} from "./utilities/auth";
-import {dbConnect} from "./utilities/dbConnection";
+//import {dbConnect} from "./utilities/dbConnection";
 import {getLogger} from 'log4js'
 
 //Enable global logger
 export const logger = getLogger();
 logger.level = process.env.LOG_LEVEL || 'info';
 
-dbConnect().then(_ => logger.info("Database connected")).catch(err => logger.error(err))
+//dbConnect().then(_ => logger.info("Database connected")).catch(err => logger.error(err))
 
 const app = express();
 const port = process.env.PORT || 3000
