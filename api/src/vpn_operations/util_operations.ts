7d3edcde6e933cf216d4c5d1263f134d3536e758
Lakindu Akash <lakinduakash@gmail.com>
2020-08-16 05:08:03 +0530

    Add util to get cert

--- api/src/vpn_operations/util_operations.ts
+++ api/src/vpn_operations/util_operations.ts
@@ -10,4 +10,23 @@ export namespace VpnUtilOperations {
         const command = `sudo ipsec statusall`
         return await ssh.execCommand(command, {cwd: '/'})
     }
+
+    export async function getCACert(): Promise<{ code, signal, stdout, stderr }> {
+        if (!ssh.connection) {
+            await connectSSH()
+            logger.info("connected to ssh")
+        }
+        const command = `cat /etc/ipsec.d/cacerts/chain.pem`
+        return await ssh.execCommand(command, {cwd: '/'})
+    }
+
+    export async function getCert(): Promise<{ code, signal, stdout, stderr }> {
+        if (!ssh.connection) {
+            await connectSSH()
+            logger.info("connected to ssh")
+        }
+        const command = `cat /etc/ipsec.d/certs/cert.pem`
+        return await ssh.execCommand(command, {cwd: '/'})
+    }
+
 }
